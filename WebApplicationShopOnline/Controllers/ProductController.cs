5c4ac413dda7122fb37aa73ded6391f7a7be2199
ddkhugaev <ddkhugaev@gmail.com>
2025-02-25 10:22:01 +0300

    яматэкудасай

---
+++ WebApplicationShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿// контроллер создавать пустой!
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string name, string description, decimal price)
+        {
+            Product product = new Product(name, description, price);
+            return product.ToString();
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class Product
+    {
+        static int IDCount = 0;
+        public string Name { get; }
+        public string Description { get; }
+        public int ProductId { get; }
+        public decimal Price { get; }
+        public Product(string name, string description, decimal price)
+        {
+            ProductId = IDCount;
+            Name = name;
+            Description = description;
+            Price = price;
+            IDCount++;
+        }
+        public override string ToString()
+        {
+            return $"{ProductId}\n{Name}\n{Description}\n{Price}";
+        }
+    }
+}
