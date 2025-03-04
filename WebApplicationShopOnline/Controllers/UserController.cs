6bd5f6edc8cc2eabdffe82cb3072de6bde60eb6c
David Khugaev <ddkhugaev@gmail.com>
2025-03-04 16:06:49 +0300

    Добавил модель, контроллер и представление для пользователя

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User("Давид", "Хугаев", "ddkhugaev", "1234");
+            return View(user);
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class User
+    {
+        static int instanceCounter = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public User(string name, string surname, string login, string password)
+        {
+            Id = instanceCounter;
+            Name = name;
+            Surname = surname;
+            Login = login;
+            Password = password;
+            instanceCounter++;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {Name}\nФамилия: {Surname}\nЛогин: {Login}\nПароль: {Password}";
+        }
+    }
+}
