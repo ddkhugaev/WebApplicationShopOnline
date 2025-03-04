8308f3fd2b8dba3dad837787898270293c676127
David Khugaev <ddkhugaev@gmail.com>
2025-03-04 10:35:23 +0300

    Добавили модель, контроллер и представление для товара

---
+++ WebApplicationShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product prod = new Product("пирог", "уалибиус", 777);
+            //ViewBag.Product = prod;
+            return View(prod);
+        }
+
+        //public string Index()
+        //{
+        //    Product prod = new Product("пирог", "уалибиус", 777);
+        //    return prod.ToString();
+        //}
+    }
+}
---
+++ WebApplicationShopOnline/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class Product
+    {
+        static int instanceCounter = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public Product(string name, string description, decimal cost)
+        {
+            Id = instanceCounter;
+            Name = name;
+            Description = description;
+            Cost = cost;
+            instanceCounter++;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {Description} - {Cost}р.";
+        }
+    }
+}
